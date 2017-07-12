7afc8a94732364433d2335bb306aebc736749d03
Brandon Stiles <stilesbr1@gmail.com>
2017-07-12 09:50:37 -0400

    chore(refactoring): Migrated to use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,8 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -13,18 +18,3 @@
 (require 'org)
 (setq org-src-fontify-natively t)
 (org-babel-load-file (expand-file-name "settings.org" "~/.emacs.d/"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-terminal-cursor-changer ack helm-config rainbow-mode epa-file org-crypt org-agenda yaml-mode web-mode web-beautify use-package solidity-mode smart-mode-line shakespeare-mode scala-mode sane-term pg org-journal org-bullets org-agenda-property org-ac nix-mode nginx-mode neotree markdown-mode magit jsx-mode intero iedit hungry-delete helm-projectile helm-flycheck helm-descbinds helm-ag gruvbox-theme grep+ git-gutter-fringe evil-org evil-leader dumb-jump dockerfile-mode docker dashboard ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(neo-dir-link-face ((t (:foreground "#FB4934"))))
- '(neo-file-link-face ((t (:foreground "#FAF4C1")))))
