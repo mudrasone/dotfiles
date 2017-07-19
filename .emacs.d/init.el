170dd19af8b8237254a9f1e772a48b072c9859dd
Brandon Stiles <stilesbr1@gmail.com>
2017-07-19 09:07:21 -0400

    chore(refactoring): Cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,4 @@
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
+(setq inhibit-startup-message t)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -12,12 +9,14 @@
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+	(package-refresh-contents)
+	(package-install 'use-package))
 
-(require 'org)
-(setq org-src-fontify-natively t)
-(org-babel-load-file (expand-file-name "settings.org" "~/.emacs.d/"))
+(use-package org
+ :config 
+ (setq org-src-fontify-natively t)
+ :init
+ (org-babel-load-file (expand-file-name "settings.org" "~/.emacs.d/")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,7 +24,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-terminal-cursor-changer evil-org evil-leader evil ack goto-chg undo-tree iedit hungry-delete grep+ ag dumb-jump neotree helm-ag helm-descbinds helm-flycheck helm-projectile helm-config helm projectile dashboard gruvbox-theme intero scala-mode smart-mode-line magit markdown-mode rainbow-mode solidity-mode shakespeare-mode web-mode yaml-mode docker dockerfile-mode nix-mode nginx-mode git-gutter-fringe web-beautify jsx-mode epa-file org-crypt org-ac org-journal org-agenda-property org-agenda org-bullets pg sane-term use-package))))
+    (evil-terminal-cursor-changer evil-org evil-leader evil ack goto-chg undo-tree iedit hungry-delete grep+ ag dumb-jump neotree helm-ag helm-descbinds helm-flycheck helm-projectile helm-config projectile dashboard gruvbox-theme intero scala-mode smart-mode-line magit markdown-mode rainbow-mode solidity-mode shakespeare-mode web-mode yaml-mode docker dockerfile-mode nix-mode nginx-mode git-gutter-fringe web-beautify jsx-mode epa-file org-crypt org-ac org-journal org-agenda-property org-agenda org-bullets pg sane-term use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
