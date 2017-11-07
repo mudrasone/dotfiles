8599fec6572b3e5d2c724af6253932d5dc169c6c
Brandon Stiles <stilesbr1@gmail.com>
2017-11-07 17:29:09 -0500

    update(refactoring): Cleanup

--- .emacs.d/personal/override.el
+++ .emacs.d/personal/override.el
@@ -1,14 +1,70 @@
-(message "Overriding...")
+;; Packages
+(prelude-require-packages '(dashboard
+                            solarized-theme
+                            org-journal
+                            org-bullets
+                            neotree))
 
-(when window-system
-  (setq frame-resize-pixelwise t)
-  (set-default-font "Hasklig")
-  (set-face-attribute 'default nil :height 150)
+;; Dashboard
+(setq dashboard-items '((recents  . 10) (bookmarks . 10) (projects . 10)))
+(dashboard-setup-startup-hook)
+
+;; Theme
+(setq x-underline-at-descent-line t)
+(load-theme 'solarized-dark)
+
+;; Neotree
+(defun my-neotree-mode-hook ()
+  (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+  (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+  (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+  (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter))
+
+(defun my-neotree-toggle ()
+  (interactive)
+  (let ((project-dir (ignore-errors (projectile-project-root)))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (progn
+        (neotree-show)
+        (if project-dir
+            (neotree-dir project-dir))
+        (if file-name
+            (neotree-find file-name))))))
+
+(setq neo-theme (if (display-graphic-p) 'nerd)
+      projectile-switch-project-action 'neotree-projectile-action
+      neo-smart-open t)
+
+(setq-default neo-show-hidden-files t)
+
+(add-hook 'neotree-mode-hook 'my-neotree-mode-hook)
+(global-set-key (kbd "C-c p n") 'neotree-toggle)
+
+;; SML
+(setq sml/theme 'respectful)
+
+;; Whitespce
+(setq prelude-whitespace nil)
+
+;; HL
+(hl-line-mode -1)
+(global-hl-line-mode -1)
+(global-diff-hl-mode -1)
+
+;; Font
+(when (window-system) (set-default-font "Fira Code"))
+
+(when nil 
   (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
                  (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
                  (36 . ".\\(?:>\\)")
                  (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
                  (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+                 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
                  (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
                  (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
                  (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
@@ -26,98 +82,17 @@
                  (119 . ".\\(?:ww\\)")
                  (123 . ".\\(?:-\\)")
                  (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)"))))
+                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+                 )
+               ))
     (dolist (char-regexp alist)
       (set-char-table-range composition-function-table (car char-regexp)
                             `([,(cdr char-regexp) 0 font-shape-gstring])))))
 
-(disable-theme 'zenburn)
-(setq prelude-theme nil)
-
-(require 'gruvbox)
-(load-theme 'gruvbox)
-
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
-(setq interprogram-cut-function 'paste-to-osx
-      interprogram-paste-function 'copy-from-osx)
-
-(require 'org-journal)
-(setq org-journal-dir "/Volumes/Turing/Dropbox/.org/journal/"
+;; Org Journal
+(setq org-journal-dir "~/Dropbox/.org/journal/"
       org-journal-file-format "%Y%m%d.org")
 
-(require 'org-bullets)
 (setq org-bullets-bullet-list '("•"))
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-(require 'org-agenda-property)
-(load-library "find-lisp")
-(setq org-agenda-files (find-lisp-find-files "/Volumes/Turing/Dropbox/.org" "\.org$")
-      org-agenda-compact-blocks t
-      org-agenda-start-on-weekday 0
-      org-agenda-property-list '("DEADLINE" "SCHEDULED")
-      org-agenda-window-setup (quote current-window)
-      org-agenda-span (quote fortnight)
-      org-agenda-skip-scheduled-if-deadline-is-shown t
-      org-agenda-skip-deadline-prewarning-if-scheduled (quote pre-scheduled)
-      org-agenda-todo-ignore-deadlines (quote all)
-      org-agenda-todo-ignore-scheduled (quote all)
-      org-agenda-sorting-strategy (quote ((agenda deadline-up priority-down)
-                                          (todo priority-down category-keep)
-                                          (tags priority-down category-keep)
-                                          (search category-keep)))
-      org-deadline-warning-days 7
-      org-log-done t
-      org-agenda-skip-scheduled-if-done t
-      org-todo-keywords '((sequence "TODO" "|" "DONE" "CANCELLED")))
-
-;; (require 'org-epa)
-(epa-file-enable)
-
-;; (require 'org-crypt)
-(setq org-tags-exclude-from-inheritance (quote ("crypt"))
-      org-crypt-key "stilesbr1@gmail.com")
-(org-crypt-use-before-save-magic)
-
-(require 'dashboard)
-(setq dashboard-items '((recents  . 10) (bookmarks . 10) (projects . 10)))
-(dashboard-setup-startup-hook)
-
-(add-hook 'org-mode-hook (function (lambda () (whitespace-mode -1))))
-
-(require 'neotree)
-(setq-default neo-show-hidden-files t)
-(setq neo-theme (if (display-graphic-p) 'nerd)
-      projectile-switch-project-action 'neotree-projectile-action
-      neo-smart-open t)
-(add-hook 'neotree-mode-hook (lambda () (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-                               (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
-                               (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-                               (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
-(global-set-key (kbd "C-x n") 'neotree-projectile-action)
-(custom-set-faces '(neo-dir-link-face ((t (:foreground "#FB4934"))))
-                  '(neo-file-link-face ((t (:foreground "#FAF4C1")))))
-
-(require 'web-beautify)
-(eval-after-load 'js2-mode '(define-key js2-mode-map (kbd "C-c C-b") 'web-beautify-js))
-(eval-after-load 'js '(define-key js-mode-map (kbd "C-c C-b") 'web-beautify-js))
-(eval-after-load 'json-mode '(define-key json-mode-map (kbd "C-c C-b") 'web-beautify-js))
-(eval-after-load 'sgml-mode '(define-key html-mode-map (kbd "C-c C-b") 'web-beautify-html))
-(eval-after-load 'web-mode '(define-key web-mode-map (kbd "C-c C-b") 'web-beautify-html))
-(eval-after-load 'css-mode '(define-key css-mode-map (kbd "C-c C-b") 'web-beautify-css))
-
-(require 'flycheck-mypy)
-(add-hook 'python-mode-hook 'flycheck-mode)
-
-(add-to-list 'flycheck-disabled-checkers 'python-flake8)
-(add-to-list 'flycheck-disabled-checkers 'python-pylint)
-
-(require 'pg)
-(setq coq-prog-name "/usr/local/bin/coqtop")
--- .emacs.d/personal/preload/override.el
+++ .emacs.d/personal/preload/override.el
@@ -1,1 +1,16 @@
+;; Theme
+(disable-theme 'zenburn)
+(setq prelude-theme nil)
+
+(set-face-attribute 'fringe nil
+                    :foreground (face-foreground 'default)
+                    :background (face-background 'default))
+
+;; Window
+(when window-system
+  (toggle-scroll-bar -1)
+  (setq frame-resize-pixelwise t)
+  (set-face-attribute 'default nil :height 150))
+
+;; Evil
 (setq evil-want-C-u-scroll t)
