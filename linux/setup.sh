5e2010fc2d5f41fabf6c2151abb1471c02055761
Pindaroso <me@pindaroso.com>
2019-03-05 09:08:33 -0500

    update: Added nvidia-docker to setup script

--- linux/setup.sh
+++ linux/setup.sh
@@ -16,7 +16,19 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 
-git config --global user.email "brandon@datitect.com"
+# Nvidia Docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi # Test
+
+# GitHub
+git config --global user.email "me@pindaroso.com"
 git config --global user.username "pindaroso"
 git config --global user.name "Pindaroso"
 
